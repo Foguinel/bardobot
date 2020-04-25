0da55cb1ef7953f6172a1516b3477784f58194c7
Foguinel <foguinel@gmail.com>
2020-04-25 17:00:07 +0000

    Atualização do bot.js e adição da host

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require("./node_modules/config.json");
 const client = new Discord.Client();
 const firebase = require("firebase");
 
